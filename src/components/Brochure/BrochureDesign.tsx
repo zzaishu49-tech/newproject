3a62af7bc6f65799868951e2a0f3957ecaafd783
zzaishu49-tech <zzaishu49@gmail.com>
2025-09-22 14:15:49 +0530

    Fix undefined projects variable in BrochureDesign

--- src/components/Brochure/BrochureDesign.tsx
+++ src/components/Brochure/BrochureDesign.tsx
@@ -22,6 +22,7 @@ export function BrochureDesign() {
   const { user } = useAuth();
   const { 
     brochureProjects, 
+    projects,
     createBrochureProject, 
     updateBrochureProject,
     saveBrochurePage,
